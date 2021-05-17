75216193919d31ce8c7be08411730f400e97bc74
Just-A-TestUser <84269800+Just-A-TestUser@users.noreply.github.com>
2021-05-17 18:28:15 +0700

    Add files via upload

--- helloName.py
+++ helloName.py
@@ -1,6 +1,13 @@
 def greeting(name):
     return f"Hello, {name}!"
 
+def multilang_greeting(lang="en"):
+    if lang == "en":
+       return f"Hello!"
+    elif lang == "ru":
+        return "Привет!"
+    else:
+        return r"¯\_(ツ)_/¯"
 
 imya = input("What is your name?\n")
 print(greeting(imya))
